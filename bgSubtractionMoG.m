807c89bef1297f4c5954ca7525cc34c075af500e
miccLi <llhpqlcsg@hotmail.com>
2016-12-06 11:07:29 +0800

    update_func_2

--- bgSubtractionMoG.m
+++ bgSubtractionMoG.m
@@ -1,16 +1,14 @@
 % This function applies Mixture of Gaussian(MoG) algorithm to subtract
 % background image from a given video/image sequence.
-function bg = bgSubtractionMoG(img, mean, w, sd, u_diff, p, rank)
+function [bg, w, mean, sd] = bgSubtractionMoG(img, w, mean, sd)
     % initialize MoG algorithm parameters
-    C = 3;          % number of gaussian components (typically 3-5)
-    M = 3;          % number of background components
     D = 2.5;        % positive deviation threshold
     alpha = 0.01;   % learning rate (between 0 and 1)
     thres = 0.5;    % foreground threshold
     sd_init = 0.001;    % initial standard deviation (for new components)
 
     % calculate difference of pixel values from mean
-    img_gray = rgb2gray(img);
+    img_gray = double(rgb2gray(img));
     img_gray_dim3 = cat(3, img_gray, img_gray, img_gray);
     u_diff = img_gray_dim3 - mean;
     
@@ -33,6 +31,7 @@ function bg = bgSubtractionMoG(img, mean, w, sd, u_diff, p, rank)
     % if no components match, create new component
     match = sum(indices_to_update, 3);
 
+    [width, height] = size(img_gray);
     for i = 1:width
         for j = 1:height
             % if no components match, create new component
@@ -43,4 +42,6 @@ function bg = bgSubtractionMoG(img, mean, w, sd, u_diff, p, rank)
             end
         end
     end
+    
+    bg = uint8(bg);
 end
