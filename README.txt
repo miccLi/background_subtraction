d05ad1c6fe84c312f5b1b29d6d000e9e185204bd
miccLi <llhpqlcsg@hotmail.com>
2016-12-06 10:45:38 +0800

    new func & data

---
+++ README.txt
@@ -0,0 +1,3 @@
+This function applies Gaussian Mixture Model(GMM) algorithm to subtract background image from a given video/image sequence.
+Paper related to GMM: Stauffer, C., Grimson, W.E.L., "Adaptive background mixture models for real-time tracking"
+A simple description of this algorithm is as follows:
--- bgSubtractionMoG.m
+++ bgSubtractionMoG.m
@@ -1,3 +1,46 @@
-function bgSubtractionMoG = [video, C, M, D, alpha, thres, sd_init]
+% This function applies Mixture of Gaussian(MoG) algorithm to subtract
+% background image from a given video/image sequence.
+function bg = bgSubtractionMoG(img, mean, w, sd, u_diff, p, rank)
+    % initialize MoG algorithm parameters
+    C = 3;          % number of gaussian components (typically 3-5)
+    M = 3;          % number of background components
+    D = 2.5;        % positive deviation threshold
+    alpha = 0.01;   % learning rate (between 0 and 1)
+    thres = 0.5;    % foreground threshold
+    sd_init = 0.001;    % initial standard deviation (for new components)
+
+    % calculate difference of pixel values from mean
+    img_gray = rgb2gray(img);
+    img_gray_dim3 = cat(3, img_gray, img_gray, img_gray);
+    u_diff = img_gray_dim3 - mean;
     
+    % update gaussian components for each pixel
+    indices_to_update = u_diff<=D*sd;
+    % update weights
+    w = (1 - alpha) * w;
+    w(indices_to_update) = w(indices_to_update) + alpha;
+    % update means and standard deviations for each gaussian distribution
+    p = alpha./w;
+    mean_new = (1-p).*mean + p.*img_gray_dim3;
+    sd_new = sqrt((1-p).*sd.^2) + p.*(img_gray_dim3-mean).^2;
+    mean(indices_to_update) = mean_new(indices_to_update);
+    sd(indices_to_update) = sd_new(indices_to_update);
+    
+    % calculate background model
+    w = w ./ cat(3, sum(w,3),sum(w,3),sum(w,3));
+    bg = sum(mean .* w, 3);
+    
+    % if no components match, create new component
+    match = sum(indices_to_update, 3);
+
+    for i = 1:width
+        for j = 1:height
+            % if no components match, create new component
+            if (match(i,j) == 0)
+                [w_min, w_index] = min(w(i,j,:));
+                mean(i,j,w_index) = img_gray(i,j);
+                sd(i,j,w_index) = sd_init;
+            end
+        end
+    end
 end
